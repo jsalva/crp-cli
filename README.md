9ca7978e233da5f88edaa886c815c2b0ebdaef65
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-04-24 10:48:30 +0100

    job progress report

--- README.md
+++ README.md
@@ -10,8 +10,10 @@ npm install -g crowdprocess-cli
 
 ## job
 
+Create a job and upload some data
+
 ```bash
-crowdprocess-cli job -d tests/fixtures/data.json -c 123 -p tests/fixtures/program.js -b 12
+$ crowdprocess-cli job -d tests/fixtures/data.json -c 123 -p tests/fixtures/program.js -b 12
 About to create job with these options:
 { bid: 12,
   client_id: 123,
@@ -21,3 +23,15 @@ Confirm? [y,n] (y)
 Job successfully created with id QszRDfLHkjKHbyIZyFQD7GEM4yL
 Upload terminated
 ```
+
+## progress
+
+Check the progress of a given job
+
+```bash
+$ crowdprocess-cli progress QszRDfLHkjKHbyIZyFQD7GEM4yL
+Progress for job QszRDfLHkjKHbyIZyFQD7GEM4yL:
+  0%
+  Total   : 1000
+  Complete: 0
+  Pending : 1000```
---
+++ commands/progress.js
@@ -0,0 +1,42 @@
+require('colors');
+var fs         = require('fs');
+var inspect    = require('util').inspect;
+var read       = require('read');
+var JSONStream = require('JSONStream');
+var multimeter = require('multimeter')
+
+var Client = require('jobs-client');
+
+exports =
+module.exports = progress;
+
+module.exports.usage =
+function usage(name, args) {
+  args.
+    alias('c', 'client').
+    demand('client');
+};
+
+function progress(args) {
+  var jobId = args._[0];
+  if (! jobId) {
+    console.error('No job id specified');
+    process.exit(-1);
+  }
+
+  var client = Client({
+    client_id: args.client
+  });
+
+  client.jobs.progress(jobId, function(err, stats) {
+    if (err) throw err;
+    var percentage = 0;
+    if (stats.total) percentage = (stats.complete / stats.total) * 100;
+    console.log('Progress for job %s:', jobId.yellow);
+    console.log('\t%d%'.green, percentage);
+    console.log('\tTotal   :\t%s', stats.total.toString().green);
+    console.log('\tComplete:\t%s', stats.complete.toString().green);
+    console.log('\tPending :\t%s', stats.pending.toString().green);
+  });
+
+}
