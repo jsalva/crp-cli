fecf57a2e4727ef69fd1e76ab2f460ca90abb750
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-05-21 15:44:43 +0100

    starting the job producing stream

--- commands/job.js
+++ commands/job.js
@@ -74,6 +74,7 @@ function proceed(options) {
     var readFile = fs.createReadStream(options.dataFilePath, 'utf8');
     var jsonStream = JSONStream.parse([true]);
     readFile.pipe(jsonStream).pipe(stream, {end: false});
+    stream.start();
 
     var finishedSending = false;
     jsonStream.once('end', function() {
