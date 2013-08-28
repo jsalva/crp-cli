1f9d95588a2a9fa634e9535e3a9b315f40e60d88
Igor Soarez <igorsoarez@gmail.com>
2013-08-28 18:43:07 +0100

    show cancel reason and state

--- commands/progress.js
+++ commands/progress.js
@@ -25,13 +25,13 @@ function progress(args, credential) {
     var percentage = 0;
     if (stats.total) percentage = (stats.complete / stats.total) * 100;
     console.log('Progress for task %s:', taskId.yellow);
-    if (stats.cancelReason)
-      console.log('\tCancel reason: %s', stats.cancelReason.red);
+
+    console.log('\tState:  :\t%s %s', stats.state, stats.cancelReason || ''); 
     console.log('\tProgress:\t%s', util.format('%d%', percentage).green);
     console.log('\tTotal   :\t%s', stats.total.toString().green);
     console.log('\tComplete:\t%s', stats.complete.toString().green);
     console.log('\tPending :\t%s', stats.pending.toString().green);
-    console.log('\tErrors: :\t%s', stats.errors.toString().green);
+    console.log('\tFaults: :\t%s', stats.faults.toString().green);
     console.log();
 
     if (percentage == 100) {
